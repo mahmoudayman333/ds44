d00c7daa27b7cfcfc8bbf98654807cb99a04e2f2
omaar <126203684+mahmoudayman333@users.noreply.github.com>
2025-12-08 23:22:43 +0200

    Add files via upload

---
+++ ds5(1).R
@@ -0,0 +1,33 @@
+install.packages("dplyr")
+library("dplyr")
+sum(is.na(Food_Delivery_Times))
+Food_Delivery_Times$Courier_Experience_yrs[is.na(Food_Delivery_Times$Courier_Experience_yrs)]<-mean(Food_Delivery_Times$Courier_Experience_yrs,na.rm=TRUE)
+Food_Delivery_Times$Weather[is.na(Food_Delivery_Times$Weather)]<-mode(Food_Delivery_Times$Weather)
+Food_Delivery_Times$Time_of_Day[is.na(Food_Delivery_Times$Time_of_Day)]<-mode(Food_Delivery_Times$Time_of_Day)
+Food_Delivery_Times$Traffic_Level[is.na(Food_Delivery_Times$Traffic_Level)]<-mode(Food_Delivery_Times$Traffic_Level)
+boxplot(Food_Delivery_Times[,7:9])
+outlier=boxplot(Food_Delivery_Times$Delivery_Time_min)$out
+print(outlier)
+Food_Delivery_Times[which(Food_Delivery_Times$Delivery_Time_min %in% outlier),]
+dd<-Food_Delivery_Times[-which(Food_Delivery_Times$Delivery_Time_min %in% outlier),]
+print(dd)
+boxplot(dd$Delivery_Time_min)
+Food_Delivery_Times<-dd
+boxplot(Food_Delivery_Times[,7:9])
+Food_Delivery_Times$Speed_kmph <- Food_Delivery_Times$Distance_km / (Food_Delivery_Times$Delivery_Time_min / 60)
+boxplot(Food_Delivery_Times[,7:10])
+outlier<-boxplot(Food_Delivery_Times$Speed_kmph)$out
+print(outlier)
+Food_Delivery_Times[which(Food_Delivery_Times$Speed_kmph %in% outlier),]
+dd<-Food_Delivery_Times[-which(Food_Delivery_Times$Speed_kmph %in% outlier),]
+print(dd)
+Food_Delivery_Times<-dd    
+boxplot(Food_Delivery_Times[,7:10])
+x = table(Food_Delivery_Times$Time_of_Day )
+per = paste0(round(100*x/sum(x)),"%")
+pie(x , labels = per ,main = "How the delivery time is dustributed throughout the day",col= c("red","blue","green","pink","purple"))
+legend("bottomright", legend = c("Evening", "Afternoon","Night","morning","character"), fill = c("red", "blue","green","pink"))
+boxplot( x = Food_Delivery_Times$Delivery_Time_min , main = "the overall spread and central tendency of the total delivery time",
+         xlab = "tendency" , ylab = "time (min)")
+
+plot( x = Food_Delivery_Times$Delivery_Time_min , y = Food_Delivery_Times$Distance_km,  xlab = "delivery time (min)" , ylab = "distance (km)" , col = "red")
